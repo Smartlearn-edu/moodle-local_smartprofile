7f65519f492629e5aa56f375cf675994eb0eb1d9
Smartlearn-edu <onlymade@gmail.com>
2026-08-17 11:49:38 +0300

    v

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_smartprofile';
-$plugin->version   = 2026081702;
+$plugin->version   = 2026081703;
 $plugin->requires  = 2024100700; // Moodle 5.0+.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0.0';
